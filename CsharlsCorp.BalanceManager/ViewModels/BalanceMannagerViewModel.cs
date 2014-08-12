9ce7bcfceb6071437c42dd8fd07c109fd4c67ed6
Carlos Garcia <carlossgarcia@outlook.com>
2014-08-12 17:04:56 -0600

    Added Action Command

---
+++ CsharlsCorp.BalanceManager/ViewModels/BalanceMannagerViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using CsharlsCorp.BalanceManager.Model;
+
+
+
+namespace CsharlsCorp.BalanceManager.ViewModels
+{
+    public class BalanceMannagerViewModel:ViewModel
+    {
+        private BManager _manager = new BManager();
+
+        private ObservableCollection<Transaction> _transactions;
+
+        public ObservableCollection<Transaction> Transactions
+        {
+            get { return _transactions; }
+            set { _transactions = value; }
+        }
+
+        private ActionCommand getTransactions;
+
+        public ActionCommand GetTransactions
+        {
+            get {
+                if (getTransactions==null)
+                {
+                    getTransactions = new ActionCommand(() =>
+                        {
+                            Transactions = new ObservableCollection<Transaction>(_manager.AllTransactions());
+                        });
+                }
+                return getTransactions; 
+            }
+            
+        }
+
+
+    }
+}
---
+++ CsharlsCorp.BalanceManager/ViewModels/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CsharlsCorp.BalanceManager.ViewModels
+{
+    public class ViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName=null)
+        {
+            if (PropertyChanged!=null)
+            {
+                PropertyChanged(this,new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
