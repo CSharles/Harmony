66075fa1788961c084a6daab7a4c1a9f68fb425f
Carlos Garcia <carlossgarcia@outlook.com>
2014-10-17 15:45:22 -0600

    Added General View and NavigateCommand

---
+++ CsharlsCorp.BalanceManager/ViewModels/NavigateComand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace CsharlsCorp.BalanceManager.ViewModels
+{
+    public class NavigateComand:ICommand
+    {
+        public BalanceMannagerViewModel  BalanceViewModel { get; set; }
+
+        public NavigateComand(BalanceMannagerViewModel viewModel) {
+            BalanceViewModel = viewModel;
+        
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            BalanceViewModel.Navigate(parameter.ToString());
+        }
+    }
+}
